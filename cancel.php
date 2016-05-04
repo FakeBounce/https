5e2f3062f1d004fafdb1e31bbd7c9dd3bf5f9198
fakebounce <benjamin.romanelli@neuf.fr>
2016-05-04 17:55:02 +0200

    first commit

---
+++ cancel.php
@@ -0,0 +1,15 @@
+<div class="hero_form">
+	<div class="container">
+		<h1 style="text-align: center;">
+			Paiement annulé
+		</h1>
+		
+		<div class="container">
+		Le paiement a été annulé. En espérant que vous changerez d'avis, nous vous adressons nos salutations les plus sincères.
+		</div>
+	</div>
+</div>
+  <?php
+
+  
+?>
